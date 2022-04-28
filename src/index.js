9aa6b76ffcf3d44bedd995b378f98841809630d1
purplevarun <purplevarun@gmail.com>
2022-04-28 19:27:21 +0530

    Made Font Bold

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import Dark from "./dark/Dark";
 import Light from "./light/Light";
 import propTypes from "prop-types";
 const defaultFontSize = 30;
-const defaultFontWidth = 800;
-const index = ({ children, fontSize, fontWidth, colorMode }) => {
+const defaultFontWeight = 800;
+const index = ({ children, fontSize, fontWeight, colorMode }) => {
 	const wrapper = {
 		style: {
 			fontSize: fontSize ? fontSize : defaultFontSize,
-			fontWidth: fontWidth ? fontWidth : defaultFontWidth,
+			fontWeight: fontWeight ? fontWeight : defaultFontWeight,
 		},
 	};
 	const helperProps = { children };
 	return (
-		<div {...wrapper}>
+		<div {...wrapper} style={{ fontWeight: "bold" }}>
 			{colorMode === "dark" ? (
 				<Dark {...helperProps} />
 			) : (
@@ -25,7 +25,7 @@ const index = ({ children, fontSize, fontWidth, colorMode }) => {
 index.PropTypes = {
 	children: propTypes.string,
 	fontSize: propTypes.number,
-	fontWidth: propTypes.number,
+	fontWeight: propTypes.number,
 	colorMode: "light" | "dark",
 };
 export default index;
