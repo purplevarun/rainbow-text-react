5b9707b291ca326a7b15ee3b74e92037353f5c2a
purplevarun <purplevarun@gmail.com>
2022-04-28 19:14:59 +0530

    Added Proptypes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Dark from "./dark/Dark";
 import Light from "./light/Light";
 const defaultFontSize = 30;
+import propTypes from "prop-types";
 const index = ({ children, fontSize, colorMode }) => {
 	const wrapper = {
 		style: {
@@ -19,5 +20,9 @@ const index = ({ children, fontSize, colorMode }) => {
 		</div>
 	);
 };
-
+index.PropTypes = {
+	children: propTypes.string,
+	fontSize: propTypes.number,
+	colorMode: "light" | "dark",
+};
 export default index;
